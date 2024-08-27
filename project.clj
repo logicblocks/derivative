4d7c5b02a1e26cc3d43989ef0f4d13c8f0ae500b
Circle CI <circleci@logicblocks.io>
2024-08-27 12:16:06 +0000

    Pre-release version 0.1.0-RC3 [skip ci]

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject io.logicblocks/derivative "0.1.0-RC2"
+(defproject io.logicblocks/derivative "0.1.0-RC3"
   :description "General purpose in place scaffolding and source code rewrite tool."
   :url "https://github.com/logicblocks/derivative"
 
